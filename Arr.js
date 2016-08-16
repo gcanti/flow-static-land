cdf5ccff72fd5774f7d367c018560d16a27be6a9
gcanti <giulio.canti@gmail.com>
2016-08-16 19:31:14 +0200

    rename List to Arr

---
+++ Arr.js
@@ -0,0 +1,46 @@
+// @flow
+import { HKT } from './HKT'
+import type { Monad } from './Monad'
+import type { Foldable } from './Foldable'
+
+class Arr {}
+
+export const t = Arr
+
+function prj<A>(fa: HKT<Arr, A>): Array<A> {
+  return ((fa: any): Array<A>)
+}
+
+export function inj<A>(a: Array<A>): HKT<Arr, A> {
+  return ((a: any): HKT<Arr, A>)
+}
+
+export function map<A, B>(f: (a: A) => B, fa: HKT<Arr, A>): HKT<Arr, B> {
+  return inj(prj(fa).map(f))
+}
+
+export function ap<A, B>(fab: HKT<Arr, (a: A) => B>, fa: HKT<Arr, A>): HKT<Arr, B> {
+  return inj(prj(fab).reduce((acc, f) => acc.concat(prj(fa).map(f)), []))
+}
+
+export function of<A>(a: A): HKT<Arr, A> {
+  return inj([a])
+}
+
+export function chain<A, B>(f: (a: A) => HKT<Arr, B>, fa: HKT<Arr, A>): HKT<Arr, B> {
+  return inj(prj(fa).reduce((acc, a) => acc.concat(prj(f(a))), []))
+}
+
+export function reduce<A, B>(f: (a: A, b: B) => A, a: A, fb: HKT<Arr, B>): A {
+  return prj(fb).reduce(f, a)
+}
+
+if (false) { // eslint-disable-line
+  ({
+    map,
+    ap,
+    of,
+    chain,
+    reduce
+  }: Monad<Arr, *, *> & Foldable<Arr, *, *>)
+}
