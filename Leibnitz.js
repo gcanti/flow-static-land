f5e1023d4738b7c6f6654c9f0b092aa2e254a99c
gcanti <giulio.canti@gmail.com>
2016-08-19 14:57:44 +0200

    add Leibnitz

---
+++ Leibnitz.js
@@ -0,0 +1,28 @@
+// @flow
+import { HKT } from './HKT'
+import { unsafeCoerce } from './Unsafe'
+import { of, extract, id } from './Identity'
+
+export type Leibnitz<A, B> = {
+  <F>(a: HKT<F, A>): HKT<F, B>
+};
+
+export function simm<A, B>(proof: Leibnitz<A, B>): Leibnitz<B, A> {
+  return unsafeCoerce(proof)
+}
+
+export function coerce<A, B>(proof: Leibnitz<A, B>): (a: A) => B {
+  return (a) => extract(proof(of(a)))
+}
+
+export function coerceSymm<A, B>(proof: Leibnitz<A, B>): (b: B) => A {
+  return coerce(simm(proof))
+}
+
+export function refl<A>(): Leibnitz<A, A> {
+  return unsafeCoerce(id)
+}
+
+export function subst<F, A, B>(proof: Leibnitz<A, B>, fa: HKT<F, A>): HKT<F, B> {
+  return proof(fa)
+}
