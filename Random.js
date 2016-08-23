fcf7b9cac4e8360498018bd1d9f88adf1968010c
Giulio Canti <giulio.canti@gmail.com>
2016-08-23 11:39:40 +0200

    add Random (#8)

---
+++ Random.js
@@ -0,0 +1,28 @@
+// @flow
+import type { Eff } from './Eff'
+import { inj, map } from './Eff'
+
+// The `RANDOM` effect indicates that an Eff action may access or modify the
+// JavaScript global random number generator, i.e. `Math.random()`.
+export class RANDOM {}
+
+export function random<E>(): Eff<E & { random: RANDOM }, number> {
+  return inj(() => Math.random())
+}
+
+// Returns a random integer between min (included) and max (included)
+export function randomInt<E>(min: number, max: number): Eff<E & { random: RANDOM }, number> {
+  const lo = Math.ceil(min)
+  const hi = Math.floor(max)
+  return map((n) => Math.floor(n * (hi - lo + 1)) + lo, random())
+}
+
+// Returns a random number between a minimum value (inclusive) and a maximum value (exclusive)
+export function randomRange<E>(min: number, max: number): Eff<E & { random: RANDOM }, number> {
+  return map((n) => Math.floor(n * (max - min)) + min, random())
+}
+
+// Returns a random boolean value with an equal chance of being `true` or `false`
+export function randomBool<E>(): Eff<E & { random: RANDOM }, boolean> {
+  return map((n) => n < 0.5, random())
+}
---
+++ test/Random.js
@@ -0,0 +1,37 @@
+// @flow
+
+declare var describe: Function;
+declare var it: Function;
+
+import assert from 'assert'
+import { runEff } from '../Eff'
+import {
+  randomInt,
+  randomRange,
+  randomBool
+} from '../Random'
+
+describe('Random', () => {
+
+  it('randomInt', () => {
+    const eff = randomInt(10, 20)
+    for (let i = 0; i < 1000; i++) {
+      const n = runEff(eff)
+      assert.ok(n >= 10 && n <= 20)
+    }
+  })
+
+  it('randomRange', () => {
+    const eff = randomRange(10, 20)
+    for (let i = 0; i < 1000; i++) {
+      const n = runEff(eff)
+      assert.ok(n >= 10 && n < 20)
+    }
+  })
+
+  it('randomBool', () => {
+    const b = runEff(randomBool())
+    assert.ok(b === true || b === false)
+  })
+
+})
