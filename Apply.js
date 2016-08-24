9e0830b449690dce221aea737ab2db0ac1082327
gcanti <giulio.canti@gmail.com>
2016-08-24 11:01:15 +0200

    add lift2

--- Apply.js
+++ Apply.js
@@ -9,3 +9,7 @@ export interface Apply<F> extends Functor<F> {
 export function ap<F, A, B>(dictApply: Apply<F>, fab: HKT<F, (a: A) => B>, fa: HKT<F, A>): HKT<F, B> {
   return dictApply.ap(fab, fa)
 }
+
+export function lift2<F, A, B, C>(apply: Apply<F>, f: (a: A, b: B) => C): (fa: HKT<F, A>, fb: HKT<F, B>) => HKT<F, C> {
+  return (fa, fb) => apply.ap(apply.map((a) => (b) => f(a, b), fa), fb)
+}
