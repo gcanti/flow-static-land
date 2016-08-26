926d576c00767ded8ad6bf94172f11b9a8697df4
Giulio Canti <giulio.canti@gmail.com>
2016-08-26 13:51:57 +0200

    add variadic compose & pipe (up to 5 args) + Fn* types (#13)
    * add variadic compose & pipe (up to 5 args) + Fn* types
    
    * add type safe curry

--- Fun.js
+++ Fun.js
@@ -1,6 +1,50 @@
 // @flow
 export type Predicate<A> = (a: A) => boolean;
 
+export type Fn1<A, B> = (a: A, ...rest: Array<void>) => B;
+export type Fn2<A, B, C> = (a: A, b: B, ...rest: Array<void>) => C;
+export type Fn3<A, B, C, D> = (a: A, b: B, c: C, ...rest: Array<void>) => D;
+export type Fn4<A, B, C, D, E> = (a: A, b: B, c: C, d: D, ...rest: Array<void>) => E;
+export type Fn5<A, B, C, D, E, F> = (a: A, b: B, c: C, d: D, e: E, ...rest: Array<void>) => F;
+export type Fn6<A, B, C, D, E, F, G> = (a: A, b: B, c: C, d: D, e: E, f: F, ...rest: Array<void>) => G;
+export type Fn7<A, B, C, D, E, F, G, H> = (a: A, b: B, c: C, d: D, e: E, f: F, g: G, ...rest: Array<void>) => H;
+export type Fn8<A, B, C, D, E, F, G, H, I> = (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, ...rest: Array<void>) => I;
+export type Fn9<A, B, C, D, E, F, G, H, I, L> = (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, ...rest: Array<void>) => L;
+
+declare function compose<A, B, C>(bc: Fn1<B, C>, ab: Fn1<A, B>, ...rest: Array<void>): Fn1<A, C>; // eslint-disable-line no-redeclare
+declare function compose<A, B, C, D>(cd: Fn1<C, D>, bc: Fn1<B, C>, ab: Fn1<A, B>, ...rest: Array<void>): Fn1<A, D>; // eslint-disable-line no-redeclare
+declare function compose<A, B, C, D, E>(de: Fn1<D, E>, cd: Fn1<C, D>, bc: Fn1<B, C>, ab: Fn1<A, B>, ...rest: Array<void>): Fn1<A, E>; // eslint-disable-line no-redeclare
+declare function compose<A, B, C, D, E, F>(fe: Fn1<F, E>, de: Fn1<D, E>, cd: Fn1<C, D>, bc: Fn1<B, C>, ab: Fn1<A, B>, ...rest: Array<void>): Fn1<A, F>; // eslint-disable-line no-redeclare
+
+declare function pipe<A, B, C>(ab: Fn1<A, B>, bc: Fn1<B, C>, ...rest: Array<void>): Fn1<A, C>; // eslint-disable-line no-redeclare
+declare function pipe<A, B, C, D>(ab: Fn1<A, B>, bc: Fn1<B, C>, cd: Fn1<C, D>, ...rest: Array<void>): Fn1<A, D>; // eslint-disable-line no-redeclare
+declare function pipe<A, B, C, D, E>(ab: Fn1<A, B>, bc: Fn1<B, C>, cd: Fn1<C, D>, de: Fn1<D, E>, ...rest: Array<void>): Fn1<A, E>; // eslint-disable-line no-redeclare
+declare function pipe<A, B, C, D, E, F>(ab: Fn1<A, B>, bc: Fn1<B, C>, cd: Fn1<C, D>, de: Fn1<D, E>, ef: Fn1<E, F>, ...rest: Array<void>): Fn1<A, F>; // eslint-disable-line no-redeclare
+
+export type CurriedFn2<A, B, C> =
+  & Fn1<A, Fn1<B, C>>
+  & Fn2<A, B, C>;
+export type CurriedFn3<A, B, C, D> =
+  & Fn1<A, CurriedFn2<B, C, D>>
+  & Fn2<A, B, Fn1<C, D>>
+  & Fn3<A, B, C, D>;
+export type CurriedFn4<A, B, C, D, E> =
+  & Fn1<A, CurriedFn3<B, C, D, E>>
+  & Fn2<A, B, CurriedFn2<C, D, E>>
+  & Fn3<A, B, C, Fn1<D, E>>
+  & Fn4<A, B, C, D, E>;
+export type CurriedFn5<A, B, C, D, E, F> =
+  & Fn1<A, CurriedFn4<B, C, D, E, F>>
+  & Fn2<A, B, CurriedFn3<C, D, E, F>>
+  & Fn3<A, B, C, CurriedFn2<D, E, F>>
+  & Fn4<A, B, C, D, Fn1<E, F>>
+  & Fn5<A, B, C, D, E, F>;
+
+declare function curry<A, B, C>(f: Fn2<A, B, C>): CurriedFn2<A, B, C>; // eslint-disable-line no-redeclare
+declare function curry<A, B, C, D>(f: Fn3<A, B, C, D>): CurriedFn3<A, B, C, D>; // eslint-disable-line no-redeclare
+declare function curry<A, B, C, D, E>(f: Fn4<A, B, C, D, E>): CurriedFn4<A, B, C, D, E>; // eslint-disable-line no-redeclare
+declare function curry<A, B, C, D, E, F>(f: Fn5<A, B, C, D, E, F>): CurriedFn5<A, B, C, D, E, F>; // eslint-disable-line no-redeclare
+
 // Flips the order of the arguments to a function of two arguments.
 export function flip<A, B, C>(f: (a: A, b: B) => C): (b: B, a: A) => C {
   return (b, a) => f(a, b)
@@ -16,10 +60,33 @@ export function on<A, B, C>(o: (x: B, y: B) => C, f: (a: A) => B): (x: A, y: A) 
   return (x, y) => o(f(x), f(y))
 }
 
-export function compose<A, B, C>(f: (b: B) => C, g: (a: A) => B): (a: A) => C {
-  return a => f(g(a))
+export function compose(...fns) { // eslint-disable-line no-redeclare
+  const len = fns.length - 1
+  return result => {
+    for (let i = len; i > -1; i--) {
+      result = fns[i].call(this, result)
+    }
+    return result
+  }
 }
 
-export function pipe<A, B, C>(f: (a: A) => B, g: (b: B) => C): (a: A) => C {
-  return a => g(f(a))
+export function pipe(...fns) { // eslint-disable-line no-redeclare
+  const len = fns.length - 1
+  return result => {
+    for (let i = 0; i <= len; i++) {
+      result = fns[i].call(this, result)
+    }
+    return result
+  }
+}
+
+function curried(f, length, acc) {
+  return function () {
+    const combined = acc.concat(Array.prototype.slice.call(arguments))
+    return combined.length >= length ? f.apply(this, combined) : curried(f, length, combined)
+  }
+}
+
+export function curry(f: Function) { // eslint-disable-line no-redeclare
+  return curried(f, f.length, [])
 }
---
+++ test/Fun.js
@@ -0,0 +1,43 @@
+// @flow
+
+declare var describe: Function;
+declare var it: Function;
+
+import assert from 'assert'
+import {
+  compose,
+  pipe,
+  curry
+} from '../Fun'
+
+const f = (n: number) => n + 1
+const g = (n: number) => n * 2
+
+describe('Fun', () => {
+
+  it('compose', () => {
+    assert.strictEqual(compose(f, g)(2), 5)
+    assert.strictEqual(compose(f, g, f)(2), 7)
+    assert.strictEqual(compose(f, g, f, g)(2), 11)
+    assert.strictEqual(compose(f, g, f, g, f)(2), 15)
+  })
+
+  it('pipe', () => {
+    assert.strictEqual(pipe(f, g)(2), 6)
+    assert.strictEqual(pipe(f, g, f)(2), 7)
+    assert.strictEqual(pipe(f, g, f, g)(2), 14)
+    assert.strictEqual(pipe(f, g, f, g, f)(2), 15)
+  })
+
+  it('curry', () => {
+    const h = curry((a: number, b: number, c: number): number => a + b + c)
+    assert.strictEqual(h(1)(2)(3), 6)
+    assert.strictEqual(h(1)(2, 3), 6)
+    assert.strictEqual(h(1, 2)(3), 6)
+    assert.strictEqual(h(1, 2, 3), 6)
+    const hh = curry((a: number, b: number, c: number, d: number, e: number): number => a + b + c + d + e)
+    assert.strictEqual(hh(1)(2)(3)(4)(5), 15)
+    assert.strictEqual(hh(1)(2, 3)(4)(5), 15)
+  })
+
+})
