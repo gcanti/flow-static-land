e426c241442a57ed1d7c4b0b23935b2aec25cd16
gcanti <giulio.canti@gmail.com>
2016-08-27 20:03:41 +0200

    add composeKleisli

--- Chain.js
+++ Chain.js
@@ -5,3 +5,16 @@ import type { Apply } from './Apply'
 export interface Chain<F> extends Apply<F> {
   chain<A, B>(f: (a: A) => HKT<F, B>, fa: HKT<F, A>): HKT<F, B>
 }
+
+// Forwards Kleisli composition.
+//
+// For example:
+//
+// ```js
+// import * as arr from '../Arr'
+//
+// const second = a => composeKleisli(maybe, arr.tail, arr.head)
+// ```
+export function composeKleisli<F, A, B, C>(chain: Chain<F>, f: (a: A) => HKT<F, B>, g: (b: B) => HKT<F, C>): (a: A) => HKT<F, C> {
+  return a => chain.chain(g, f(a))
+}
