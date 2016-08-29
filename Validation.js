19e18259ff365846dc9b74c63ebeb6ef03668f72
gcanti <giulio.canti@gmail.com>
2016-08-29 15:24:58 +0200

    add applicative functor for applicative validation

---
+++ Validation.js
@@ -0,0 +1,22 @@
+// @flow
+import type { Applicative } from './Applicative'
+import type { Semigroup } from './Semigroup'
+import type { Either, EitherF } from './Either'
+
+import * as either from './Either'
+
+export function validationApplicative<L>(semigroup: Semigroup<L>): Applicative<EitherF> {
+  function ap<A, B>(fab: Either<L, (a: A) => B>, fa: Either<L, A>): Either<L, B> {
+    const ab = either.prj(fab)
+    const a = either.prj(fa)
+    if (ab instanceof either.Left && a instanceof either.Left) {
+      return either.left(semigroup.concat(ab.value0, a.value0))
+    }
+    return either.ap(fab, fa)
+  }
+  return {
+    map: either.map,
+    ap,
+    of: either.of
+  }
+}
