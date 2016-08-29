4344b370e3cfd4348f832666f327032d02f15bb5
gcanti <giulio.canti@gmail.com>
2016-08-29 15:16:36 +0200

    rename lift2 to liftA2

--- Apply.js
+++ Apply.js
@@ -6,7 +6,7 @@ export interface Apply<F> extends Functor<F> {
   ap<A, B>(fab: HKT<F, (a: A) => B>, fa: HKT<F, A>): HKT<F, B>
 }
 
-export function lift2<F, A, B, C>(apply: Apply<F>, f: (a: A, b: B) => C): (fa: HKT<F, A>, fb: HKT<F, B>) => HKT<F, C> {
+export function liftA2<F, A, B, C>(apply: Apply<F>, f: (a: A, b: B) => C): (fa: HKT<F, A>, fb: HKT<F, B>) => HKT<F, C> {
   const cf = a => b => f(a, b)
   return (fa, fb) => apply.ap(apply.map(cf, fa), fb)
 }
