6a549d246716662ce7425c80ffa38777bf7d1c18
gcanti <giulio.canti@gmail.com>
2016-08-31 14:24:44 +0200

    add some Monoid basic instances

--- Monoid.js
+++ Monoid.js
@@ -4,3 +4,28 @@ import type { Semigroup } from './Semigroup'
 export interface Monoid<A> extends Semigroup<A> {
   empty(): A
 }
+
+export const booleanAndMonoid: Monoid<boolean> = {
+  empty: () => true,
+  concat: (x, y) => x && y
+}
+
+export const booleanOrMonoid: Monoid<boolean> = {
+  empty: () => false,
+  concat: (x, y) => x || y
+}
+
+export const numberAdditionMonoid: Monoid<number> = {
+  empty: () => 0,
+  concat: (x, y) => x + y
+}
+
+export const numberProductMonoid: Monoid<number> = {
+  empty: () => 1,
+  concat: (x, y) => x * y
+}
+
+export const stringMonoid: Monoid<string> = {
+  empty: () => '',
+  concat: (x, y) => x + y
+}
