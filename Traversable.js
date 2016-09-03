a906751efbeb1753361d471dd06f216910ebaeb9
gcanti <giulio.canti@gmail.com>
2016-09-03 11:46:00 +0200

    fix Traversable definition

--- Traversable.js
+++ Traversable.js
@@ -1,7 +1,9 @@
 // @flow
 import { HKT } from './HKT'
+import type { Functor } from './Functor'
+import type { Foldable } from './Foldable'
 import type { Applicative } from './Applicative'
 
-export interface Traversable<T> {
+export interface Traversable<T> extends Functor<T>, Foldable<T> {
   sequence<F, A>(applicative: Applicative<F>, tfa: HKT<T, HKT<F, A>>): HKT<F, HKT<T, A>>
 }
