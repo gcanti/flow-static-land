e36cd55b8f8541e81828ec39964bdb6f5ccbec91
gcanti <giulio.canti@gmail.com>
2017-02-27 12:33:08 +0100

    add liftA3, liftA4

--- src/Apply.js
+++ src/Apply.js
@@ -10,3 +10,13 @@ export function liftA2<F, A, B, C>(apply: Apply<F>, f: (a: A, b: B) => C): (fa: 
   const cf = a => b => f(a, b)
   return (fa, fb) => apply.ap(apply.map(cf, fa), fb)
 }
+
+export function liftA3<F, A, B, C, D>(apply: Apply<F>, f: (a: A, b: B, c: C) => D): (fa: HKT<F, A>, fb: HKT<F, B>, fc: HKT<F, C>) => HKT<F, D> {
+  const cf = a => b => c => f(a, b, c)
+  return (fa, fb, fc) => apply.ap(apply.ap(apply.map(cf, fa), fb), fc)
+}
+
+export function liftA4<F, A, B, C, D, E>(apply: Apply<F>, f: (a: A, b: B, c: C, d: D) => E): (fa: HKT<F, A>, fb: HKT<F, B>, fc: HKT<F, C>, fd: HKT<F, D>) => HKT<F, E> {
+  const cf = a => b => c => d => f(a, b, c, d)
+  return (fa, fb, fc, fd) => apply.ap(apply.ap(apply.ap(apply.map(cf, fa), fb), fc), fd)
+}
